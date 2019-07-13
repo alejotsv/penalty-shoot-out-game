e0eec5d11734a982f3e804c6cfc3106069a2b05c
Alejandro Salas <asalas.wd@outlook.com>
2019-07-13 14:47:07 -0400

    create canvas with background image

--- js/main.js
+++ js/main.js
@@ -6,7 +6,14 @@ $( document ).ready(function() {
     gameBoard.append(`<canvas id='my-canvas' width='800' height='600' style="border:1px solid #000000;"></canvas>`);
     const myCanvas = document.getElementById("my-canvas");
     const ctx = myCanvas.getContext("2d");
-    
+    // Create canvas, set height and width
+
+    let backgroundImg = new Image();
+    backgroundImg.src = './img/background-goal.jpg';
+
+    backgroundImg.onload = () => {
+      ctx.drawImage(backgroundImg, 0, 0, 800, 600);
+    };
   }
   
   

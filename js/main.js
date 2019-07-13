84b7553a29371a5b508dcc4bafa420a51e778aba
Alejandro Salas <asalas.wd@outlook.com>
2019-07-13 13:02:49 -0400

    add onclick to create canvas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
-  let button = $('#startGame');
+  let button = $('#start-game');
   
   function startGame() {
     let gameBoard = $('#board');
-    gameBoard.append(`<canvas id='myCanvas' width='500' height='640' style="border:1px solid #000000;"></canvas>`);
-    let canvas = $('#myCanvas');    
-    let ctx = canvas.getContext("2d");
+    gameBoard.append(`<canvas id='my-canvas' width='800' height='600' style="border:1px solid #000000;"></canvas>`);
+    const myCanvas = document.getElementById("my-canvas");
+    const ctx = myCanvas.getContext("2d");
     
   }
   
